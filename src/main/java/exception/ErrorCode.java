36e1df91003f4033c6f6a6699374201182e3c113
gracia10 <gracia10@daum.net>
2020-05-29 10:48:01 +0900

    [fix] 400 error msg fixed

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -2,7 +2,7 @@ package exception;
 
 public enum ErrorCode {
 	
-	BAD_REQUEST(400,"C001","잘못된 주소입니다."),
+	BAD_REQUEST(400,"C001","잘못된 요청입니다."),
 	SESSION_EXPIRED(403,"C002","로그인 정보가 없습니다. 다시 접속해 주세요."),
     ACCESS_DENIED(403,"C003","접근 권한이 없습니다."),
     NOT_FOUND(404,"C004","요청하신 페이지를 찾을 수 없습니다. 관련 문의사항은 고객센터에 말씀해주세요."),
